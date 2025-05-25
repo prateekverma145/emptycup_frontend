ed2a03f58c08ce6dfbfeef7ba4358e43f90458e4
Prateek verma <148776148+prateekverma145@users.noreply.github.com>
2025-05-25 15:25:40 +0530

    Update DesignerList.jsx

--- src/components/DesignerList.jsx
+++ src/components/DesignerList.jsx
@@ -5,25 +5,30 @@ import { fetchDesigners } from "../services/api";
 export default function DesignerList({ showShortlisted }) {
   const [designers, setDesigners] = useState([]);
   const [shortlistedDesigners, setShortlistedDesigners] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const data = await fetchDesigners();
         setDesigners(data);
+
         // Optionally initialize some shortlist IDs:
         // setShortlistedDesigners(new Set(data.slice(0,2).map(d => d._id)));
       } catch (err) {
         console.error("Error fetching designers:", err);
       }
+      setLoading(false);
     };
     fetchData();
   }, []);
-
+if(designers.length === 0){
+  return <div className=" w-full h-[50px] font-chivo text-[20px] font-bold flex justify-center">No designers found</div>;
+}
   return (
     <div className="flex flex-col">
       {
-        showShortlisted ? (
+       !loading?( showShortlisted ? (
           <div className="flex flex-col ">
             {shortlistedDesigners.map((designer,index) => {
               
@@ -56,7 +61,7 @@ export default function DesignerList({ showShortlisted }) {
             })}
           </div>
         )
-
+):<div className=" w-full h-[50px] font-chivo text-[20px] font-bold flex justify-center">Fetching data...</div>
       }
     </div>
   );
